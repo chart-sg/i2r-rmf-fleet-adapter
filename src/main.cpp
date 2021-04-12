08b484aabb7924903c26203e469453631b468307
junhaochng <jun_hao_chng@cgh.com.sg>
2021-04-12 16:37:40 +0800

    Code compiles and builds with #include<rmf_fleet_adapter/agv/Adapter.hpp>

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
 #include <rmf_fleet_adapter/agv/Adapter.hpp>
 
-int main()
+class Axolotl_Fleet //: public rmf_fleet_adapter::agv::Adapter 
 {
-    rmf_fleet_adapter::agv::Adapter fc;
+public:
+    Axolotl_Fleet()
+    {
+        // return 0;
+        // rmf_fleet_adapter::agv::Adapter fleet_adapter;
+        // fleet_adapter::init_and_make("axolotl_fleet");
+    }
+};
+
+int main(int argc, char * argv[])
+{
+    // rclcpp::init(argc, argv);
+    // rclcpp::spin(std::make_shared<Axolotl_Fleet>());
+    // rclcpp::shutdown();
+     
     return 0;
 }

2c1d8a8288f563731f65225afebc50cbb05b8704
junhaochng <jun_hao_chng@cgh.com.sg>
2021-04-13 13:14:03 +0800

    Copying is the highest form of flattery... So we copy paste OSRC

---
+++ build/axolotl/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jh/Dev/barkin_mad_ws/src/axoltl
+# Build directory: /home/jh/Dev/barkin_mad_ws/src/axoltl/build/axolotl
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

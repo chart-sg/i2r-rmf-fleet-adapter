4a45ed41241b4d50f8541975525562f2439635cb
kj <keai_jiang_chee@cgh.com.sg>
2021-05-05 12:21:20 +0800

    Added real-hardware fleet adapter testing launch

--- src/client.cpp
+++ src/client.cpp
@@ -196,102 +196,102 @@ connection_metadata::ptr websocket_endpoint::get_metadata(int id) const{
     }
 }
 
-int main() {
-    bool done = false;
-    std::string input;
-    websocket_endpoint endpoint;
+// int main() {
+//     bool done = false;
+//     std::string input;
+//     websocket_endpoint endpoint;
 
-    while (!done) {
-        std::cout << "Enter Command: ";
-        std::getline(std::cin, input);
+//     while (!done) {
+//         std::cout << "Enter Command: ";
+//         std::getline(std::cin, input);
 
-        if (input == "quit") {
-            done = true;
-        } else if (input == "help") {
-            std::cout
-                << "\nCommand List:\n"
-                << "connect <ws uri>\n"
-                << "send <connection id> <message>\n"
-                << "close <connection id> [<close code:default=1000>] [<close reason>]\n"
-                << "show <connection id>\n"
-                << "help: Display this help text\n"
-                << "quit: Exit the program\n"
-                << std::endl;
-        } else if (input.substr(0,7) == "connect") {
-            int id = endpoint.connect(input.substr(8));
-            if (id != -1) {
-                std::cout << "> Created connection with id " << id << std::endl;
-            }
-        } else if (input.substr(0,4) == "send") {
-            std::stringstream ss(input);
+//         if (input == "quit") {
+//             done = true;
+//         } else if (input == "help") {
+//             std::cout
+//                 << "\nCommand List:\n"
+//                 << "connect <ws uri>\n"
+//                 << "send <connection id> <message>\n"
+//                 << "close <connection id> [<close code:default=1000>] [<close reason>]\n"
+//                 << "show <connection id>\n"
+//                 << "help: Display this help text\n"
+//                 << "quit: Exit the program\n"
+//                 << std::endl;
+//         } else if (input.substr(0,7) == "connect") {
+//             int id = endpoint.connect(input.substr(8));
+//             if (id != -1) {
+//                 std::cout << "> Created connection with id " << id << std::endl;
+//             }
+//         } else if (input.substr(0,4) == "send") {
+//             std::stringstream ss(input);
             
-            std::string cmd;
-            int id;
-            std::string message;
+//             std::string cmd;
+//             int id;
+//             std::string message;
             
-            ss >> cmd >> id;
-            std::getline(ss,message);
+//             ss >> cmd >> id;
+//             std::getline(ss,message);
             
-            endpoint.send(id, message);
-        } else if (input.substr(0,5) == "close") {
-            std::stringstream ss(input);
+//             endpoint.send(id, message);
+//         } else if (input.substr(0,5) == "close") {
+//             std::stringstream ss(input);
             
-            std::string cmd;
-            int id;
-            int close_code = websocketpp::close::status::normal;
-            std::string reason;
+//             std::string cmd;
+//             int id;
+//             int close_code = websocketpp::close::status::normal;
+//             std::string reason;
             
-            ss >> cmd >> id >> close_code;
-            std::getline(ss,reason);
+//             ss >> cmd >> id >> close_code;
+//             std::getline(ss,reason);
             
-            endpoint.close(id, close_code, reason);
-        } else if (input.substr(0,4) == "show") {
-            int id = atoi(input.substr(5).c_str());
+//             endpoint.close(id, close_code, reason);
+//         } else if (input.substr(0,4) == "show") {
+//             int id = atoi(input.substr(5).c_str());
 
-            connection_metadata::ptr metadata = endpoint.get_metadata(id);
-            if (metadata) {
-                // Removed the << operator overload
-                // std::cout << *metadata << std::endl;
-            } else {
-                std::cout << "> Unknown connection id " << id << std::endl;
-            }
-        } 
-        else if (input.substr(0,5) == "dummy") {
-            int id = endpoint.connect("https://mrccc.chart.com.sg:5100");
-            if (id != -1) {
-                std::cout << "> Created connection with id " << id << std::endl;
-            }
+//             connection_metadata::ptr metadata = endpoint.get_metadata(id);
+//             if (metadata) {
+//                 // Removed the << operator overload
+//                 // std::cout << *metadata << std::endl;
+//             } else {
+//                 std::cout << "> Unknown connection id " << id << std::endl;
+//             }
+//         } 
+//         else if (input.substr(0,5) == "dummy") {
+//             int id = endpoint.connect("https://mrccc.chart.com.sg:5100");
+//             if (id != -1) {
+//                 std::cout << "> Created connection with id " << id << std::endl;
+//             }
 
-            sleep(1);
-            std::string idme_cmd = mrccc_utils::mission_gen::identifyMe();
-            std::cout << "Identify me!" << std::endl;
-            std::cout << idme_cmd << std::endl;
-            endpoint.send(id, idme_cmd);
+//             sleep(1);
+//             std::string idme_cmd = mrccc_utils::mission_gen::identifyMe();
+//             std::cout << "Identify me!" << std::endl;
+//             std::cout << idme_cmd << std::endl;
+//             endpoint.send(id, idme_cmd);
 
-            sleep(1);
-            std::string initpose_cmd = mrccc_utils::mission_gen::initRobotPose();
-            std::cout << "Initialise pose!" << std::endl;
-            std::cout << initpose_cmd << std::endl;
-            endpoint.send(id, initpose_cmd);
+//             sleep(1);
+//             std::string initpose_cmd = mrccc_utils::mission_gen::initRobotPose();
+//             std::cout << "Initialise pose!" << std::endl;
+//             std::cout << initpose_cmd << std::endl;
+//             endpoint.send(id, initpose_cmd);
 
-            while(1)
-            {
-                sleep(1);
-                // connection_metadata::ptr metadata = endpoint.get_metadata(0);
-                // std::cout<<"The size of the vector in the feedback is "<<metadata->m_messages.size()<<std::endl;
-            }
-        }
-        else {
-            std::cout << "> Unrecognized Command" << std::endl;
-        }
+//             while(1)
+//             {
+//                 sleep(1);
+//                 // connection_metadata::ptr metadata = endpoint.get_metadata(0);
+//                 // std::cout<<"The size of the vector in the feedback is "<<metadata->m_messages.size()<<std::endl;
+//             }
+//         }
+//         else {
+//             std::cout << "> Unrecognized Command" << std::endl;
+//         }
 
-        // Checking feedback
+//         // Checking feedback
         
-        // for (const auto & msg : metadata->m_messages){
-        //     std::cout<<msg<<std::endl;
-        //     sleep(1);
-        // } 
-    }
+//         // for (const auto & msg : metadata->m_messages){
+//         //     std::cout<<msg<<std::endl;
+//         //     sleep(1);
+//         // } 
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
